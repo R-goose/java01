ea8be46b3651ad2d360e275e8030094967ae7ff9
烧鹅 <1582630916@qq.com>
2024-04-22 20:07:07 +0800

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,178 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button fillingColor;
+
+    @FXML
+    private Button searching;
+
+    @FXML
+    private Button deleteNode;
+
+    @FXML
+    private Button openOne;
+
+    @FXML
+    private Button center_layout;
+
+    @FXML
+    private Button rightHide;
+
+    @FXML
+    private Button viewing;
+
+    @FXML
+    private Button left_layout;
+
+    @FXML
+    private Button leftHide;
+
+    @FXML
+    private Button setting;
+
+    @FXML
+    private Button saving;
+
+    @FXML
+    private Button deriving;
+
+    @FXML
+    private Button GetNewOne;
+
+    @FXML
+    private Button right_layout;
+
+    @FXML
+    private Button borderRadious;
+
+    @FXML
+    private Button newNode;
+
+    @FXML
+    private Button fontSize;
+
+    @FXML
+    private Button fontColor;
+
+    @FXML
+    void setting(ActionEvent event) {
+        System.out.println(666);
+    }
+
+
+    @FXML
+    void GetNewOne(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void openOne(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void viewing(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void searching(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void saving(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void deriving(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newNode(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void deleteNode(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void left_layout(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void right_layout(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void center_layout(ActionEvent event) {
+
+    }
+
+
+
+    @FXML
+    void borderRadious(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void fillingColor(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void fontSize(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void fontColor(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void leftHide(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void rightHide(ActionEvent event) {
+
+    }
+
+
+}
